01681a953f758cea8bff10df1a15d85e9566f0ba
linyuzhou <linyuzhou@lixiang.com>
2022-12-05 12:31:28 +0800

    update config:
        1. add alacritty.
        2. modify lualine.
    
    Signed-off-by: linyuzhou <linyuzhou@lixiang.com>

--- nvim/lua/conf/lualine.lua
+++ nvim/lua/conf/lualine.lua
@@ -3,8 +3,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -22,8 +22,8 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    --lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_x = {'encoding'},
+    lualine_x = {'encoding', 'filetype', 'fileformat'},
+    --lualine_x = {'encoding'},
     lualine_y = {},
     --lualine_y = {'progress'},
     --lualine_z = {'location'}
